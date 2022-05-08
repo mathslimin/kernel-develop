1caa434f815c2c18a7219b46502c9aa8bccbc005
mathslimin <mathslimin@gmail.com>
2022-05-08 22:39:00 +0800

    Create global.sh

---
+++ global.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export SHELL_DIR=$(pwd)
+#export TOP_DIR=$(dirname "$PWD")
+export TOP_DIR=$SHELL_DIR
+#export SRC_DIR=$TOP_DIR/src
+export WORK_DIR=$TOP_DIR/build
+export IMAGE_DIR=$WORK_DIR/images
+export LOG_PATH=$TOP_DIR/log
