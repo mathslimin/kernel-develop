209e94bf3741f70ef6bd9cdef4ec4699c7d340c9
mathslimin <mathslimin@gmail.com>
2022-05-08 22:39:24 +0800

    Update build_busybox.sh

--- build_busybox.sh
+++ build_busybox.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 set -x
+source ./global.sh
 
 build_arm64() {
 	echo "start to build ARM64 $1 kernel image!!"
 	export ARCH=arm64
 	export CROSS_COMPILE=aarch64-linux-gnu-
 	cp ../configs/busybox/configs/qemu_arm64_defconfig configs/
-	make qemu_arm64_defconfig 
+	make qemu_arm64_defconfig
 	make install
 }
 
@@ -16,22 +17,37 @@ build_arm() {
 	export ARCH=arm
 	export CROSS_COMPILE=arm-linux-gnueabi-
 	cp ../configs/busybox/configs/qemu_arm_defconfig configs/
-	make qemu_arm_defconfig 
+	make qemu_arm_defconfig
+	make install
+}
+
+
+build_x86_64() {
+	echo "start to build x86_64 $1 kernel image!!"
+	export ARCH=x86_64
+	export CC=gcc
+	cp ../configs/busybox/configs/qemu_x86_64_defconfig configs/
+	make ARCH=$ARCH CC=$CC qemu_x86_64_defconfig
+	#make ARCH=$ARCH CC=$CC defconfig
+	#make ARCH=$ARCH CC=$CC menuconfig # Select "Build static binary"
 	make install
 }
 
 
 #main entry
 
-PLATFORM=$1
+arch=$1
 cd busybox-1.35.0
 
-case ${PLATFORM} in
+case ${arch} in
 	arm64)
 		build_arm64 
 		;;
 	arm)
 		build_arm 
+		;;
+	x86_64)
+		build_x86_64
 		;;
 	clean)
 		echo "start to clean!!"
@@ -44,6 +60,7 @@ case ${PLATFORM} in
 		echo "eg:"
 		echo "   ./build.sh arm64     #build default  arm64 config"
 		echo "   ./build.sh arm       #build default arm config"
+		echo "   ./build.sh x86_64       #build default x86_64 config"
 		exit 1
 		;;
 esac
