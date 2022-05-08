3eaa9ba88a83ccbddfddea78f11e775997b905d1
mathslimin <mathslimin@gmail.com>
2022-05-08 22:42:31 +0800

    Update qemu_sd_start.sh

--- qemu_sd_start.sh
+++ qemu_sd_start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+set -e
+set -x
+source ./global.sh
 arch=$1
 
 ROOTDIR=`pwd`
@@ -8,34 +10,35 @@ if [ "${arch}" = "" ];then
 	arch=arm64
 fi
 
-# generate rootfs.ext3
-cd rootfs/${arch}
-mkdir -p sdcard
-sudo dd if=/dev/zero of=rootfs.ext3 bs=1M count=50
-sudo mkfs.ext3 rootfs.ext3
-sudo mount -t ext3 rootfs.ext3 sdcard -o loop
-sudo cp _install/* sdcard/ -fra
-sudo umount sdcard/
-sudo chmod 666 rootfs.ext3
-
-cd ${ROOTDIR}
+cd linux-5-kernel
 
 if [ "${arch}" = "arm" ];then
 	qemu-system-arm -M vexpress-a9 \
 		-smp 4 \
 		-m 1024m \
-		-kernel linux-5-kernel/arch/arm/boot/zImage \
+		-kernel arch/arm/boot/zImage \
 		-append "root=/dev/mmcblk0 rw console=ttyAMA0 loglevel=8" \
-		-dtb linux-5-kernel/arch/arm/boot/dts/vexpress-v2p-ca9.dtb \
-		-sd rootfs/${arch}/rootfs.ext3 \
+		-dtb arch/arm/boot/dts/vexpress-v2p-ca9.dtb \
+		-sd $WORK_DIR/images/${arch}/rootfs.ext3 \
 		-nographic
-else
+elif [ "${arch}" = "arm64" ];then
 	qemu-system-aarch64 -machine virt \
 		-cpu cortex-a57 \
 		-machine type=virt \
-		-nographic -m 2048 \
+		-m 2048 \
 		-smp 2 \
-		-kernel linux-5-kernel/arch/arm64/boot/Image \
+		-kernel arch/arm64/boot/Image \
 		-append "root=/dev/mmcblk0 rw console=ttyAMA0 loglevel=8  trace_event=sched:*,timer:*,irq:* trace_buf_size=40M" \
-		-sd rootfs/${arch}/rootfs.ext3
+		-sd $WORK_DIR/images/${arch}/rootfs.ext3 \
+		-nographic
+else
+	qemu-system-x86_64 \
+		-m 2G \
+		-smp 2 \
+		-kernel arch/x86/boot/bzImage \
+		-append "console=ttyS0 root=/dev/sda rw earlyprintk=serial net.ifnames=0" \ #必须root用/dev/sda否则会报错，rw表示可以读写
+		-drive file=$WORK_DIR/images/${arch}/rootfs.ext3,format=raw \
+		-net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22 \
+		-net nic,model=e1000 \
+		-nographic
 fi
