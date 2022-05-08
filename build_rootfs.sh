8eea8acc5ac6b0dd07a1bf33bc4ca4db3858f5f2
mathslimin <mathslimin@gmail.com>
2022-05-08 22:40:09 +0800

    Update build_rootfs.sh

--- build_rootfs.sh
+++ build_rootfs.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 set -e
 set -x
+source ./global.sh
 
-PLATFORM=$1
+arch=$1
+#TOP_DIR=`pwd`
+#WORK_DIR=$TOP_DIR/build
+sudo rm -r -f $WORK_DIR/rootfs
+mkdir -p $WORK_DIR/rootfs
+# ./build_busybox.sh $arch
 
-./build_busybox.sh $PLATFORM
-
-if [ $? != 0 ]; then
-	echo "Busybox Build Error!!"
-	exit 1
-fi
+# if [ $? != 0 ]; then
+# 	echo "Busybox Build Error!!"
+# 	exit 1
+# fi
 
 echo current dir:`pwd`
 
-if [ "$PLATFORM" = "arm64" ]; then
-	cd rootfs/arm64/_install
-else
-	cd rootfs/arm/_install
-fi
+# if [ "$arch" = "arm64" ]; then
+# 	cp -r ${WORK_DIR}/busybox/arm64/_install $WORK_DIR/rootfs/arm64
+# elif [ "$arch" = "arm" ]; then
+# 	cp -r ${WORK_DIR}/busybox/arm/_install $WORK_DIR/rootfs/arm
+# else
+# 	cp -r ${WORK_DIR}/busybox/x86_64/_install $WORK_DIR/rootfs/x86_64
+# fi
 
+cp -r ${WORK_DIR}/busybox/$arch/_install $WORK_DIR/rootfs/$arch
 
+cd $WORK_DIR/rootfs/$arch
 mkdir etc
 mkdir dev
 mkdir mnt
 mkdir -p etc/init.d/
-
+mkdir -p home/workspace
+if [ -d "${WORK_DIR}/ltp" ]; then
+    has_test=true
+    sudo cp -r ${WORK_DIR}/ltp home/workspace/
+fi
 # create rcS for target board
 cat > etc/init.d/rcS << EOF
 mkdir -p /proc
@@ -63,5 +75,22 @@ cd dev
 sudo mknod console c 5 1
 sudo mknod null c 1 3
 
+
+# generate rootfs.ext3
+#cd rootfs/${arch}
+sudo rm -r -f  $WORK_DIR/images
+mkdir -p $WORK_DIR/images/${arch}
+cd $WORK_DIR/images/${arch}
+mkdir -p sdcard
+if [ -d "${WORK_DIR}/ltp" ]; then
+	sudo dd if=/dev/zero of=rootfs.ext3 bs=1M count=2048
+else
+	sudo dd if=/dev/zero of=rootfs.ext3 bs=1M count=50
+fi
+sudo mkfs.ext3 rootfs.ext3
+sudo mount -t ext3 rootfs.ext3 sdcard -o loop
+sudo cp -fra $WORK_DIR/rootfs/${arch}/* sdcard/ 
+sudo umount sdcard/
+sudo chmod 666 rootfs.ext3
 
 echo "Build rootfs success!!"
