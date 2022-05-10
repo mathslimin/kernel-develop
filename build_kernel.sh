96d317a5053c6a8b82d6a03b9162aa71a65c1101
mathslimin <mathslimin@gmail.com>
2022-05-11 00:07:58 +0800

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,61 +5,59 @@ source ./global.sh
 
 build_arm64() {
 	echo "start to build ARM64 $1 kernel image!!"
-	export ARCH=arm64
-	export CROSS_COMPILE=aarch64-linux-gnu-
-	cp ../configs/arch/arm64/configs/qemu_defconfig arch/arm64/configs/
-	make $1 
-	make -j$(nproc)
+	# export ARCH=arm64
+	# export CROSS_COMPILE=aarch64-linux-gnu-
+	toolchain_arm64
+	make mrproper
+	make defconfig
+	make Image -j$(nproc)
 }
 
 build_arm() {
 	echo "start to build ARM $1 kernel image!!"
-	export ARCH=arm
-	export CROSS_COMPILE=arm-linux-gnueabi-
-	cp ../configs/arch/arm/configs/qemu_defconfig arch/arm/configs/
-	make $1 
+	# export ARCH=arm
+	# export CROSS_COMPILE=arm-none-linux-gnueabihf-
+	toolchain_arm
+	make mrproper
+	make defconfig
 	make bzImage -j$(nproc)
 	make dtbs
 }
 
 build_x86_64() {
 	echo "start to build x86_64 $1 kernel image!!"
-	export ARCH=x86_64
-	#export CROSS_COMPILE=arm-linux-gnueabi-
-	export CC=gcc
-	cp ../configs/arch/x86/configs/qemu_defconfig arch/x86/configs/
-	#make ARCH=x86_64 x86_64_defconfig #使用默认config
-	#make ARCH=x86_64 menuconfig
-	#please change .config CONFIG_RETPOLINE=n
-	make ARCH=x86_64 CC=gcc qemu_defconfig
-	make ARCH=x86_64 CC=gcc bzImage -j$(nproc)
+	# export ARCH=x86_64
+	# export CROSS_COMPILE=${GCC_X86_PATH}/bin/x86_64-unknown-linux-gnu-
+	toolchain_x86_64
+	make mrproper
+	make x86_64_defconfig CROSS_COMPILE=$CROSS_COMPILE
+	make bzImage CROSS_COMPILE=$CROSS_COMPILE -j$(nproc)
 }
 
 #main entry
 
-arch=$1
-cd linux-kernel
+export arch=$1
+cd src/linux-next
 if [ -e arch/arm64/boot/Image -a "${arch}" != "arm64" ]; then
 	echo "arch/arm64/boot/Image exist, make distclean"
-	rm arch/arm64/boot/Image -f
+	make clean
 	make distclean
 elif [ -e arch/arm/boot/zImage -a "${arch}" != "arm" ]; then
 	echo "arch/arm/boot/zImage exist, make distclean"
-	rm arch/arm/boot/zImage -f
+	make clean
 	make distclean
 elif [ -e arch/x86/boot/zImage -a "${arch}" != "x86_64" ]; then
 	echo "arch/x86/boot/zImage exist, make distclean"
-	rm arch/x86/boot/zImage -f
+	make clean
 	make distclean
 fi
 
-
 case ${arch} in
 	arm64)
-		build_arm64 qemu_defconfig
+		build_arm64
 		;;
 	arm)
-		build_arm qemu_defconfig
+		build_arm
 		;;
 	x86_64)
 		build_x86_64
@@ -68,10 +66,9 @@ case ${arch} in
 		echo "start to select config to build kernel image!!"
 		i=0
 		arch=($(ls arch/))
-		for file in ${arch[@]}
-		do
-			echo ${i}:${file##*/};
-			((i++));
+		for file in ${arch[@]}; do
+			echo ${i}:${file##*/}
+			((i++))
 		done
 		echo please input your choice:
 		read index
@@ -79,10 +76,9 @@ case ${arch} in
 
 		i=0
 		configs=($(ls arch/${target_arch}/configs/*))
-		for file in ${configs[@]}
-		do
-			echo ${i}:${file##*/};
-			((i++));
+		for file in ${configs[@]}; do
+			echo ${i}:${file##*/}
+			((i++))
 		done
 
 		echo please input your choice:
@@ -90,16 +86,17 @@ case ${arch} in
 		target_config=${configs[${index}]##*/}
 		echo target is: ${target_arch} ${target_config}
 
-		if [ ${target_arch} = "arm64" ];then
+		if [ ${target_arch} = "arm64" ]; then
 			build_arm64 ${target_config}
 		else
 			build_arm ${target_config}
-		fi	
+		fi
 		;;
 	clean)
 		echo "start to clean!!"
 		rm arch/arm64/boot/Image -f
 		rm arch/arm/boot/zImage -f
+		make clean
 		make distclean
 		;;
 	*)
@@ -113,5 +110,3 @@ case ${arch} in
 		echo "   ./build.sh select    #select platform and config to build"
 		;;
 esac
-
-
