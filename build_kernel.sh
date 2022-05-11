b7a4f6851182b23bb5d640d77838e5f23f7f685d
mathslimin <mathslimin@gmail.com>
2022-05-11 22:46:20 +0800

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,94 +3,55 @@ set -e
 set -x
 source ./global.sh
 
-build_arm64() {
-	echo "start to build ARM64 $1 kernel image!!"
-	# export ARCH=arm64
-	# export CROSS_COMPILE=aarch64-linux-gnu-
-	toolchain_arm64
-	make mrproper
-	make defconfig
-	make Image -j$(nproc)
+build_aarch64() {
+	export ARCH=arm64
+	toolchain_aarch64
+	make olddefconfig ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
+	make Image -j$(nproc) CROSS_COMPILE=$CROSS_COMPILE
 }
 
 build_arm() {
-	echo "start to build ARM $1 kernel image!!"
-	# export ARCH=arm
-	# export CROSS_COMPILE=arm-none-linux-gnueabihf-
+	export ARCH=arm
 	toolchain_arm
-	make mrproper
-	make defconfig
-	make bzImage -j$(nproc)
-	make dtbs
+	make olddefconfig ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
+	make bzImage -j$(nproc) CROSS_COMPILE=$CROSS_COMPILE
+	make dtbs CROSS_COMPILE=$CROSS_COMPILE
 }
 
 build_x86_64() {
-	echo "start to build x86_64 $1 kernel image!!"
-	# export ARCH=x86_64
-	# export CROSS_COMPILE=${GCC_X86_PATH}/bin/x86_64-unknown-linux-gnu-
+	export ARCH=x86_64
 	toolchain_x86_64
-	make mrproper
-	make x86_64_defconfig CROSS_COMPILE=$CROSS_COMPILE
-	make bzImage CROSS_COMPILE=$CROSS_COMPILE -j$(nproc)
+	make olddefconfig ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
+	make bzImage  -j$(nproc) CROSS_COMPILE=$CROSS_COMPILE
 }
 
 #main entry
 
-export arch=$1
-cd src/linux-next
-if [ -e arch/arm64/boot/Image -a "${arch}" != "arm64" ]; then
+export PLATFORM=$1
+cd $SRC_DIR/linux-next
+if [ -e arch/arm64/boot/Image -a "${PLATFORM}" != "aarch64" ]; then
 	echo "arch/arm64/boot/Image exist, make distclean"
 	make clean
 	make distclean
-elif [ -e arch/arm/boot/zImage -a "${arch}" != "arm" ]; then
+elif [ -e arch/arm/boot/zImage -a "${PLATFORM}" != "arm" ]; then
 	echo "arch/arm/boot/zImage exist, make distclean"
 	make clean
 	make distclean
-elif [ -e arch/x86/boot/zImage -a "${arch}" != "x86_64" ]; then
+elif [ -e arch/x86/boot/zImage -a "${PLATFORM}" != "x86_64" ]; then
 	echo "arch/x86/boot/zImage exist, make distclean"
 	make clean
 	make distclean
 fi
 
-case ${arch} in
-	arm64)
-		build_arm64
+case ${PLATFORM} in
+	aarch64)
+		build_aarch64
 		;;
 	arm)
 		build_arm
 		;;
 	x86_64)
 		build_x86_64
-		;;
-	select)
-		echo "start to select config to build kernel image!!"
-		i=0
-		arch=($(ls arch/))
-		for file in ${arch[@]}; do
-			echo ${i}:${file##*/}
-			((i++))
-		done
-		echo please input your choice:
-		read index
-		target_arch=${arch[${index}]##*/}
-
-		i=0
-		configs=($(ls arch/${target_arch}/configs/*))
-		for file in ${configs[@]}; do
-			echo ${i}:${file##*/}
-			((i++))
-		done
-
-		echo please input your choice:
-		read index
-		target_config=${configs[${index}]##*/}
-		echo target is: ${target_arch} ${target_config}
-
-		if [ ${target_arch} = "arm64" ]; then
-			build_arm64 ${target_config}
-		else
-			build_arm ${target_config}
-		fi
 		;;
 	clean)
 		echo "start to clean!!"
@@ -104,7 +65,7 @@ case ${arch} in
 		echo "./build.sh [platform]"
 		echo " "
 		echo "eg:"
-		echo "   ./build.sh arm64     #build default  arm64 config"
+		echo "   ./build.sh aarch64     #build default  aarch64 config"
 		echo "   ./build.sh arm       #build default arm config"
 		echo "   ./build.sh x86_64       #build default arm config"
 		echo "   ./build.sh select    #select platform and config to build"
