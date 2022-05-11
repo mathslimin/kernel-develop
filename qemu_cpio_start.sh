b0d92e935ba446a4064b16d90116ed5917be963f
mathslimin <mathslimin@gmail.com>
2022-05-11 22:48:19 +0800

    Update qemu_cpio_start.sh

--- qemu_cpio_start.sh
+++ qemu_cpio_start.sh
@@ -3,37 +3,37 @@ source ./global.sh
 set -e
 set -x
 
-arch=$1
+PLATFORM=$1
 
 ROOTDIR=`pwd`
 
-if [ "${arch}" = "" ];then
-	arch=arm64
+if [ "${PLATFORM}" = "" ];then
+	PLATFORM=arm64
 fi
 
 # generate cpio initramfs
 #cd rootfs/${arch}/_install
-cd $WORK_DIR/rootfs/${arch}
+cd $BUILD_DIR/rootfs/${PLATFORM}
 
-find . | cpio -H newc -ov --owner root:root > $IMAGE_DIR/${arch}/initramfs.cpio
+find . | cpio -H newc -ov --owner root:root > $IMAGE_DIR/${PLATFORM}/initramfs.cpio
 
 #cd ../
-cd $IMAGE_DIR/${arch}
+cd $IMAGE_DIR/${PLATFORM}
 rm -f initramfs.cpio.gz
 gzip -qq initramfs.cpio
 
-cd ${ROOTDIR}/linux-kernel
+cd ${ROOTDIR}/linux-next
 
-if [ "${arch}" = "arm" ];then
+if [ "${PLATFORM}" = "arm" ];then
 	qemu-system-arm -M vexpress-a9 \
 		-smp 2 \
 		-m 1024M \
 		-kernel arch/arm/boot/zImage \
 		-append "rdinit=/linuxrc console=ttyAMA0 loglevel=8" \
 		-dtb arch/arm/boot/dts/vexpress-v2p-ca9.dtb \
-		-initrd $IMAGE_DIR/${arch}/initramfs.cpio.gz \
+		-initrd $IMAGE_DIR/${PLATFORM}/initramfs.cpio.gz \
 		-nographic
-elif [ "${arch}" = "arm64" ];then
+elif [ "${PLATFORM}" = "arm64" ];then
 	qemu-system-aarch64 -machine virt \
 		-cpu cortex-a57 \
 		-machine type=virt \
@@ -41,23 +41,16 @@ elif [ "${arch}" = "arm64" ];then
 		-smp 2 \
 		-kernel arch/arm64/boot/Image \
 		-append "rdinit=/linuxrc console=ttyAMA0 loglevel=8  trace_event=sched:*,timer:*,irq:* trace_buf_size=40M" \
-		-initrd $IMAGE_DIR/${arch}/initramfs.cpio.gz \
+		-initrd $IMAGE_DIR/${PLATFORM}/initramfs.cpio.gz \
 		-nographic
 else
 	# 必须root用/dev/sda否则会报错，rw表示可以读写
-	# qemu-system-x86_64 \
-	# 	-m 2048 \
-	# 	-smp 2 \
-	# 	-kernel arch/x86/boot/bzImage \
-	# 	-append "rdinit=/linuxrc console=ttyAMA0 loglevel=8  trace_event=sched:*,timer:*,irq:* trace_buf_size=40M" \
-	# 	-initrd $IMAGE_DIR/${arch}/initramfs.cpio.gz \
-	# 	-nographic
 	qemu-system-x86_64 \
 		-smp 2 \
 		-m 1024m \
 		-kernel arch/x86/boot/bzImage \
 		-append "console=ttyS0 root=/dev/sda rw earlyprintk=serial net.ifnames=0" \
-		-drive file=$IMAGE_DIR/${arch}/initramfs.cpio.gz,format=raw \
+		-drive file=$IMAGE_DIR/${PLATFORM}/initramfs.cpio.gz,format=raw \
 		-net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22 \
 		-net nic,model=e1000 \
 		-nographic
