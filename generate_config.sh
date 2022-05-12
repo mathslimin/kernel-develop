6382a2bb398d37e0e19f7ad1cdf6d99405c5bad9
mathslimin <mathslimin@gmail.com>
2022-05-12 21:13:01 +0800

    Update generate_config.sh

--- generate_config.sh
+++ generate_config.sh
@@ -2,12 +2,24 @@
 set -e
 set -x
 source ./global.sh
+#main entry
+
+export PLATFORM=$1
+if [ 0 = $# ]; then
+    usage
+    exit
+fi
 
 build_aarch64() {
 	export ARCH=arm64
 	toolchain_aarch64
 	make mrproper
 	make defconfig CROSS_COMPILE=$CROSS_COMPILE
+	sed -i 's/^# CONFIG_KCOV is not set/CONFIG_KCOV=y/1' .config
+	sed -i "/CONFIG_LKDTM/aCONFIG_KASAN=y" .config
+	sed -i 's/^CONFIG_CMDLINE=\"\"/CONFIG_CMDLINE=\"console=ttyAMA0\"/1' .config
+	sed -i "/CONFIG_LKDTM/aCONFIG_KCOV_INSTRUMENT_ALL=y" .config
+	make menuconfig
 }
 
 build_arm() {
@@ -15,6 +27,7 @@ build_arm() {
 	toolchain_arm
 	make mrproper
 	make defconfig CROSS_COMPILE=$CROSS_COMPILE
+	make menuconfig
 }
 
 build_x86_64() {
@@ -22,22 +35,29 @@ build_x86_64() {
 	toolchain_x86_64
 	make mrproper
 	make x86_64_defconfig CROSS_COMPILE=$CROSS_COMPILE
+	sed -i 's/^# CONFIG_KCOV is not set/CONFIG_KCOV=y/1' .config
+	sed -i "/CONFIG_LKDTM/aCONFIG_KASAN=y" .config
+	sed -i 's/^CONFIG_CMDLINE=\"\"/CONFIG_CMDLINE=\"console=ttyAMA0\"/1' .config
+	sed -i "/CONFIG_LKDTM/aCONFIG_KCOV_INSTRUMENT_ALL=y" .config
+	sed -i 's/^CONFIG_E1000=m/CONFIG_E1000=y/1' .config
+	sed -i 's/^CONFIG_E1000E=m/CONFIG_E1000E=y/1' .config
+	make menuconfig
 }
 
-#main entry
-
-export PLATFORM=$1
 cd $SRC_DIR/linux-next
 if [ -e arch/arm64/boot/Image -a "${PLATFORM}" != "aarch64" ]; then
 	echo "arch/arm64/boot/Image exist, make distclean"
+	rm -f arch/arm64/boot/Image
 	make clean
 	make distclean
 elif [ -e arch/arm/boot/zImage -a "${PLATFORM}" != "arm" ]; then
 	echo "arch/arm/boot/zImage exist, make distclean"
+	rm -f arch/arm/boot/zImage
 	make clean
 	make distclean
 elif [ -e arch/x86/boot/zImage -a "${PLATFORM}" != "x86_64" ]; then
 	echo "arch/x86/boot/zImage exist, make distclean"
+	rm -f arch/x86/boot/zImage
 	make clean
 	make distclean
 fi
