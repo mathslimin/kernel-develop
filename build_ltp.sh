2f6cea141a14c16563a926047e5e6c8a7a09573b
mathslimin <mathslimin@gmail.com>
2022-05-12 21:12:16 +0800

    Update build_ltp.sh

--- build_ltp.sh
+++ build_ltp.sh
@@ -6,23 +6,12 @@ source ./global.sh
 # default compile
 #./configure CC=arm-linux-gnueabi-gcc --build=i686-pc-linux-gnu --target=arm-linux --host=arm-linux  CFLAGS="-static" LDFLAGS="-static  -pthread"
 
-function usage() {
-    echo ""
-    echo "usage:"
-    echo "  ./build_ltp.sh arm"
-    echo ""
-    exit 1
-}
 if [ 0 = $# ]; then
     usage
     exit
 fi
 
 export PLATFORM=$1
-
-if [ "${PLATFORM}" = "" ]; then
-    usage
-fi
 mkdir -p ${BUILD_DIR}
 rm -r -f ${BUILD_DIR}/ltp
 cd src/ltp
@@ -33,15 +22,15 @@ if [ "${PLATFORM}" = "arm" ]; then
     toolchain_arm
     ./configure --prefix=${BUILD_DIR}/ltp CC=${GCC_PATH} --host=${TARGET_HOST}
     make -j$(nproc)
-    make install
+    make install -j$(nproc)
 elif [ "${PLATFORM}" = "aarch64" ]; then
     toolchain_aarch64
     ./configure --prefix=${BUILD_DIR}/ltp CC=${GCC_PATH} --host=${TARGET_HOST}
     make -j$(nproc)
-    make install
+    make install -j$(nproc)
 else
     toolchain_x86_64
     ./configure --prefix=${BUILD_DIR}/ltp CC=${GCC_PATH} --host=${TARGET_HOST}
     make -j$(nproc)
-    make install
+    make install -j$(nproc)
 fi
