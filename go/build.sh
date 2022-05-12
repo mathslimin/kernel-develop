552aaf241d58f352597acee2094b9a45cc6bac81
mathslimin <mathslimin@gmail.com>
2022-05-12 21:10:33 +0800

    Update build.sh

--- go/build.sh
+++ go/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 go mod tidy
-GOOS=linux GOARCH=$1 go build -a -ldflags '-extldflags \"-static\"' sshd_server.go
-GOOS=linux GOARCH=$1 go build -a -ldflags '-extldflags \"-static\"' client.go
+GOOS=linux GOARCH=$1 go build -a -ldflags '-extldflags \"-static\"' -o bin/sshd_server sshd_server.go 
+GOOS=linux GOARCH=$1 go build -a -ldflags '-extldflags \"-static\"' -o bin/client client.go
+GOOS=linux GOARCH=$1 go build -a -ldflags '-extldflags \"-static\"' -o bin/http_server http_server.go 
