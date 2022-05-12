f5d42f3e87268f76ae93b347d47d0c82e9b93163
mathslimin <mathslimin@gmail.com>
2022-05-12 21:11:46 +0800

    Update build_examples.sh

--- build_examples.sh
+++ build_examples.sh
@@ -2,40 +2,29 @@
 set -e
 #set -x
 source ./global.sh
-function usage() {
-    echo ""
-    echo "usage:"
-    echo "  ./build_ltp.sh arm"
-    echo ""
-    exit 1
-}
 if [ 0 = $# ]; then
     usage
     exit
 fi
 
 export PLATFORM=$1
-
-if [ "${PLATFORM}" = "" ]; then
-    usage
-fi
 toolchain_${PLATFORM}
 export CC=${GCC_PATH}
 export CXX=${CXX_PATH}
 
 build_aarch64() {
-	toolchain_aarch64
-	make
+    toolchain_aarch64
+    make
 }
 
 build_arm() {
-	toolchain_arm
-	make
+    toolchain_arm
+    make
 }
 
 build_x86_64() {
-	toolchain_x86_64
-	make
+    toolchain_x86_64
+    make
 }
 
 #main entry
@@ -43,21 +32,21 @@ cd examples
 pwd
 make clean
 case ${PLATFORM} in
-	aarch64)
-		build_aarch64
-		;;
-	arm)
-		build_arm
-		;;
-	x86_64)
-		build_x86_64
-		;;
-	clean)
-		echo "start to clean!!"
-		make clean
-		;;
-	*)
-		echo "usage:"
-		echo "./build_examples.sh [platform]"
-		;;
+    aarch64)
+        build_aarch64
+        ;;
+    arm)
+        build_arm
+        ;;
+    x86_64)
+        build_x86_64
+        ;;
+    clean)
+        echo "start to clean!!"
+        make clean
+        ;;
+    *)
+        echo "usage:"
+        echo "./build_examples.sh [platform]"
+        ;;
 esac
