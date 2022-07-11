df8d2e3c7d68a9b83f080b0193208bca2a4c76c1
mathslimin <mathslimin@gmail.com>
2022-07-11 10:08:45 +0800

    Update copy_to_rootfs.sh

--- copy_to_rootfs.sh
+++ copy_to_rootfs.sh
@@ -1,29 +1,64 @@
-#!/bin/bash
-set -x
-export GCC_ARM_PATH=/opt/buildtools/gcc-linaro-arm-linux-gnueabihf
-#export GCC_ARM_PATH=/home/paas/software/gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf
-#export GCC_ARM_PATH=/home/paas/software/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf
-export GCC_PATH=$GCC_ARM_PATH/bin/arm-linux-gnueabihf-gcc
-#sudo umount rootfs
+#!/usr/bin/env bash
+set -e
+function help() {
+    echo "usage: ./copy_to_rootfs.h ARCH machineName"
+    echo "example: ./copy_to_rootfs.h x86_64 linux-x86_64"
+}
+if [ 0 = $# ] || [ 1 = $# ]; then
+    help
+    exit
+fi
+export PLATFORM=$1
+export MACHINE_NAME=$2
+source ./global.sh
+which guestmount || {
+    echo "guestmount is not exist, sudo apt install libguestfs-tools"
+    sudo apt-get install -y libguestfs-tools
+}
+
+export IMAGE_DIR=${QEMU_HOME_DIR}/images/${MACHINE_NAME}
+cd $IMAGE_DIR
+
 sudo rm -r -f rootfs
-ROOTFS=./rootfs
-rm rootfs.ext2
-cp backup_rootfs.ext2 rootfs.ext2
+ROOTFS=${IMAGE_DIR}/rootfs
 mkdir -p rootfs
-sleep 1
-sudo mount -o loop rootfs.ext2 rootfs
-#sudo cp -r /home/paas/workspace/shell/hulk_test/qemu/output/ltp/ $ROOTFS/opt/
-sudo cp -r ./ltp/ $ROOTFS/opt/
-sudo cp /home/paas/workspace/shell/hulk_test/qemu/examples/helloworld $ROOTFS/opt/
-sudo mkdir -p $ROOTFS/usr/local/libc/usr/lib
-sudo mkdir -p $ROOTFS/usr/local/libc/lib
-#sudo mkdir -p $ROOTFS/usr/lib
-#sudo mkdir -p $ROOTFS/lib
-
-#sudo cp -prv $($GCC_PATH -print-sysroot)/lib/*so* $ROOTFS/usr/local/libc/lib/
-#sudo cp -prv $($GCC_PATH -print-sysroot)/usr/lib/*so* $ROOTFS/usr/local/libc/usr/lib
-#sudo cp -prv $($GCC_PATH -print-sysroot)/lib/*so* $ROOTFS/lib/
-#sudo cp -prv $($GCC_PATH -print-sysroot)/usr/lib/*so* $ROOTFS/usr/lib/
-
-#echo 'export LD_LIBRARY_PATH=/usr/local/libc/lib:/usr/local/libc/usr/lib:$LD_LIBRARY_PATH' | sudo tee --append $ROOTFS/etc/profile
-sudo umount rootfs
+sleep 3
+if [ -f ${IMAGE_DIR}/rootfs.ext4 ]; then
+    if [ ! -f ${IMAGE_DIR}/backup_rootfs.ext4 ]; then
+        cp ${IMAGE_DIR}/rootfs.ext4 ${IMAGE_DIR}/backup_rootfs.ext4
+    fi
+    sudo mount -o loop ${IMAGE_DIR}/rootfs.ext4 rootfs
+elif [ -f ${IMAGE_DIR}/rootfs.qcow2 ]; then
+    if [ ! -f ${IMAGE_DIR}/backup_rootfs.qcow2 ]; then
+        cp ${IMAGE_DIR}/rootfs.qcow2 ${IMAGE_DIR}/backup_rootfs.qcow2
+    fi
+    sudo guestmount -a ${IMAGE_DIR}/rootfs.qcow2 -m /dev/sda2 rootfs
+else
+    echo "Error: rootfs not exists"
+    exit 1
+fi
+if [ -d "${INSTALL_DIR}/ltp" ]; then
+    sudo cp -r ${INSTALL_DIR}/ltp rootfs/opt/
+fi
+if [ -f "${TOP_DIR}/examples/helloworld" ]; then
+    sudo mkdir -p rootfs/opt/bin
+    sudo cp -r ${TOP_DIR}/examples/helloworld rootfs/opt/bin/
+fi
+if [ -d "${INSTALL_DIR}/lib" ]; then
+    sudo rm -f ${INSTALL_DIR}/lib/module/build
+    sudo rm -f ${INSTALL_DIR}/lib/module/source
+    sudo rm -r -f rootfs/lib/modules
+    sudo cp -r ${INSTALL_DIR}/lib/modules rootfs/lib/
+fi
+TS=$(date '+%Y%m%d%H%M%S' | sed 's/-//g')
+echo $TS >rootfs/root/deploy.info
+if [ -f ${IMAGE_DIR}/rootfs.ext4 ]; then
+    sudo umount rootfs
+elif [ -f ${IMAGE_DIR}/rootfs.qcow2 ]; then
+    sudo guestunmount rootfs
+else
+    echo "Error: rootfs not exists"
+    exit 1
+fi
+echo "success replace rootfs"
+ls -lh ${IMAGE_DIR}
